8331d8d65215e417e0fe5ca031d721d1e2b6e96d
Vyacheslav Grinin <vyacheslav.grinin@ptnl.moscow>
2021-03-14 00:35:54 +0300

    Prefabs, initial objects

---
+++ Assets/Scripts/FoodGeneration.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodGeneration : MonoBehaviour
+{
+
+    public GameObject foodPrefab;
+
+    public float xSize = 8.8f;
+    public float zSize = 8.8f;
+
+    public GameObject currentFood;
+
+    public Vector3 currentPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        randomPosition();
+        currentFood = Instantiate(foodPrefab, currentPosition, Quaternion.identity);
+    }
+
+    void randomPosition()
+    {
+        currentPosition = new Vector3(Random.Range(-xSize, xSize), 0.25f, Random.Range(-zSize, zSize));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
